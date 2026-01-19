c338b1ff77b5fbb336f3fcafd2d81fdccd5234b7
iter8 <ankit.sharma@intelliticks.com>
2026-01-19 13:14:48 +0530

    Pagination export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
 export { default as Select } from "./Select";
 export { default as RadioGroup } from "./RadioGroup";
+export { default as Pagination } from "./Pagination";
